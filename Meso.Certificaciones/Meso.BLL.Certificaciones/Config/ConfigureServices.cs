3af55d53e47f26546460396059bde1367ded9c7a
CMGSAY <carlos.garcia.say@gmail.com>
2025-10-30 02:33:20 -0600

    Se agregaron las Clases ConfigureMaps y ConfigureServices

---
+++ Meso.Certificaciones/Meso.BLL.Certificaciones/Config/ConfigureServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meso.BLL.Certificaciones.Config
+{
+    internal class ConfigureServices
+    {
+    }
+}
