7632a38460cfe956e48e7205ca432691ad30dcbf
CMGSAY <carlos.garcia.say@gmail.com>
2025-11-16 12:56:55 -0600

    Se agregaron los datos para ConfigureSwaggerOptions

--- Meso.Certificaciones/Meso.SVL.Certificaciones/Infraestructure/ConfigureSwaggerOptions.cs
+++ Meso.Certificaciones/Meso.SVL.Certificaciones/Infraestructure/ConfigureSwaggerOptions.cs
@@ -1,6 +1,41 @@
-﻿namespace Meso.SVL.Certificaciones.Infraestructure
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Meso.SVL.Certificaciones.Infraestructure
 {
-    public class ConfigureSwaggerOptions
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
     {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var descripcion in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(descripcion.GroupName, InformacionVersionApi(descripcion));
+            }
+        }
+
+        public static OpenApiInfo InformacionVersionApi(ApiVersionDescription descripcion)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "API Meso Certificaciones.",
+                Version = descripcion.ApiVersion.ToString(),
+                Description = "Documentacion",
+                Contact = new OpenApiContact() { Name = "Carlos y Eliada" }
+            };
+
+            if (descripcion.IsDeprecated)
+            {
+                info.Description += "Esta version de la API es la mas pro";
+            }
+            return info;
+        }
     }
 }
