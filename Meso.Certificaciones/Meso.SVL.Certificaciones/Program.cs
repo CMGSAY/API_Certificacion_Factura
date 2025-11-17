e9e49ee42345e9e96db6e22553dbc9db2fd03b72
EliPrez <eliadaperez@umes.edu.gt>
2025-11-16 19:16:02 -0600

    Actualizaciones Programs

--- Meso.Certificaciones/Meso.SVL.Certificaciones/Program.cs
+++ Meso.Certificaciones/Meso.SVL.Certificaciones/Program.cs
@@ -1,4 +1,17 @@
+using Asp.Versioning.ApiExplorer;
+using Meso.BLL.Certificaciones.Config;
+using Meso.BSV.Certificaciones.Config;
+using Meso.SVL.Certificaciones.Config;
+
+
 var builder = WebApplication.CreateBuilder(args);
+const string CORS_POLICY = "CorsPolicy";
+var corsValue = builder.Configuration.GetSection(CORS_POLICY).Value;
+
+// configuraciones de capas 
+builder.Services.AddBLLConfig();
+builder.Services.AddBSVConfig();
+builder.Services.AddSVLConfig(corsValue);
 
 // Add services to the container.
 
@@ -8,15 +21,32 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var route = "api-certificaciones";
+    var documentName = "{documentName}";
+    var servicesName = "CertificacionesAPI";
+
+    app.UseSwagger(options => { options.RouteTemplate = $"{route}/{documentName}/{servicesName}.json"; });
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = route;
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/{route}/{description.GroupName}/{servicesName}.json",
+              description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsValue);
+
+
 
 app.UseAuthorization();
 
